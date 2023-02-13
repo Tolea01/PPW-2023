23e92dbc268e7477768cd4f7eaac95388dc69872
Tolea2001 <anatoliecurbat@gmail.com>
2023-02-13 15:59:57 +0200

    1

---
+++ Homework/13.02.2023/Arrays/1.js
@@ -0,0 +1,41 @@
+const array = [12, 34, 'JavaScript', 5.3];
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+
+// let i = 0;
+// while (i < array.length) {
+//   console.log(array[i]);
+//   i++;
+// }
+
+
+
+// let i = 0;
+// do {
+//   console.log(array[i]);
+//   i++;
+// }
+// while (i < array.length);
+
+
+
+// for (const i in array) {
+//   console.log(array[i]);
+// }
+
+
+
+// for (const i of array) {
+//   console.log(i);
+// }
+
+
+// array.forEach((i) => console.log(i));
+
+
+// array.map((i) => console.log(i));
+
+// I liked working with the forEach and map methods
