1c4a8af7491595a8bbf2e3679e6044dd40658ed7
Tolea2001 <anatoliecurbat@gmail.com>
2023-02-13 16:01:17 +0200

    2

---
+++ Homework/13.02.2023/Arrays/2.js
@@ -0,0 +1,24 @@
+const arr = [1, 5, 0, -5, 30, 100, -500, 0, 100, 55];
+
+// 1-----------------------------------------------------------------------------
+// const greaterThan = arr.filter((elem) => elem < 5);
+
+// console.log(greaterThan);
+
+// 2-------------------------------------------------------------------------------
+// const maxElement = Math.max(...arr);
+// const minElement = Math.min(...arr);
+
+// console.log(`Index of min element: ${arr.indexOf(minElement)}\nIndex of max elememnt: ${arr.indexOf(maxElement)}`);
+
+
+// 3-------------------------------------------------------------------------------
+//   console.log(arr.sort((a, b) => b - a))
+
+
+// 4--------------------------------------------------------------------------------------------------------------
+// const sum = arr.reduce((prev, current) => prev + current, 0);
+// const arithmeticValue = sum / arr.length;
+// const result = arr.filter((elem) => elem > arithmeticValue);
+
+// console.log(result); 
